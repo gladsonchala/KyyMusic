0cf1325df24f50ac11c5bbdc2281830240f71cef
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2024-07-21 04:49:30 +0300

    Update filters.py

--- Music/MusicUtilities/helpers/filters.py
+++ Music/MusicUtilities/helpers/filters.py
@@ -1,13 +1,9 @@
-from typing import List
-from typing import Union
+from typing import List, Union
 from pyrogram import filters
 from Music.config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~ filters.edited & \
-    ~ filters.via_bot & ~ filters.forwarded
-other_filters2 = filters.private & ~ filters.edited \
-    & ~ filters.via_bot & ~ filters.forwarded
-
+other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
+other_filters2 = filters.private & ~filters.via_bot & ~filters.forwarded
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
